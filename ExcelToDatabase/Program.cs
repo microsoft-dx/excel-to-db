f5d04f278fecfc7aacc14da3d79e192238f0442a
Alexandru Petrescu <xander@Alexandrus-MacBook-Pro.local>
2020-01-07 17:59:46 +0200

    Added Base Solution

---
+++ ExcelToDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExcelToDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
